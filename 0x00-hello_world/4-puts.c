4ad141853f022bdf1d50679a9031f6f824449390
Febyemad <Febyemad@example.com>
2023-07-13 12:43:26 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.n>
+/**
+ *  main - A programme that print aline with puts functio 
+  n 
+ *Return: 0 (sucess )
+*/
+int main (void)
+{
+puts("/"Programming is like building a multilingual puzz
+		le");
+return (0);
+}
+       

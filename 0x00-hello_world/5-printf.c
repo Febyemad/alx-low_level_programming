df9568f5e0801cd908bc625ed3ec9125a2694353
Febyemad <Febyemad@example.com>
2023-07-13 15:15:03 -0700

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - A programmer that print the printf function
  * Return: 0 (Sucess)
-*/
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n")
 		return (0);
 }
 

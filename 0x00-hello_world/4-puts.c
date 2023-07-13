58b5238f457519fea48f623787c4755968ca712d
Febyemad <Febyemad@example.com>
2023-07-13 13:27:37 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.n>
 /**
- *  main - A programme that print aline with puts functio 
-  n 
- *Return: 0 (sucess )
-*/
-int main (void)
+ * main - A programm that print a line with puts function
+ * return : 0 (sucess)
+ */
+int main(void)
 {
-puts("/"Programming is like building a multilingual puzz
-		le");
+puts("\"programming is like pizzle");
 return (0);
 }
-       
+

d1175b172145ad0c09742560ef2b31499afe5bc3
Febyemad <Febyemad@example.com>
2023-07-14 11:11:18 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: prints all single digit numbers of base 10
+ * Return: 0
+ */
+int main(void)
+{
+        int c = 0;
+
+	while (c <= 10)
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
